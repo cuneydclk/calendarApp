887c34069b3d9ebedfb24e73851eee89fb6957c0
cuneydclk <cuneydclk@gmail.com>
2023-07-13 14:43:51 +0300

    a

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const connection = mongoose.createConnection('mongodb+srv://admin:<password>@atlascluster.1jdpdew.mongodb.net/?retryWrites=true&w=majority').on('open', ()=>{
+const connection = mongoose.createConnection('mongodb+srv://admin:<1234>@atlascluster.1jdpdew.mongodb.net/?retryWrites=true&w=majority').on('open', ()=>{
 console.log("MangoDb connected");
 
 }).on('error', ()=> {
