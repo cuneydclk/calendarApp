1dada28ef3e68a021764ec055b838ab24d5d84f6
cuneydclk <cuneydclk@gmail.com>
2023-07-13 13:24:32 +0300

    deployment config

--- date_app/lib/main.dart
+++ date_app/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     home: LoginPage(),
   ));
--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/',(req,res)=>{
 );
 
 app.listen(port, () => {
-  console.log(`Server Listening on Port http://localhost:${port}`);
+  console.log(`Server Listening on Port https://calenderapp.onrender.com`);
 });
